c9e4393bfa7d176b075132d9e94292b15cda2436
vladislav602 <martynnazar11@gmail.com>
2025-02-23 15:55:46 +0200

    vg

---
+++ asdf.py
@@ -0,0 +1,15 @@
+import json
+data = {
+
+    "ім'я": "Влад",
+    "Вік": "15",
+    "Улюблені предмети": [
+        "фізика"
+        "хімія"
+
+    ]
+
+}
+
+with open ("data.json", "w", encoding="utf-8") as file:
+    json.dump(data, file,ensure_ascii=False, indent=4)
---
+++ example3.py
@@ -0,0 +1,2 @@
+with open("example.txt", "a", encoding="utf-8") as file:
+    file.write("\n36")
